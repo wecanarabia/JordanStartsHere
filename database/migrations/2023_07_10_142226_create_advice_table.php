f2d2b91d01f411db403dc225f01165a41fb821f4
mina <mina899432@gmail.com>
2023-07-12 13:07:51 +0300

    intro

--- database/migrations/2023_07_10_142226_create_advice_table.php
+++ database/migrations/2023_07_10_142226_create_advice_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('advice', function (Blueprint $table) {
+        Schema::create('advices', function (Blueprint $table) {
             $table->id();
             $table->text('body');
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('advice');
+        Schema::dropIfExists('advices');
     }
 };
