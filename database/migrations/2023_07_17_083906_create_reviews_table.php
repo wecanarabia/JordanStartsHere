718073c66657724fabd6aaef046cce166f34e19c
aya-matroud <ayamatroud8@gmail.com>
2023-07-17 04:42:46 -0700

    addReviews

---
+++ database/migrations/2023_07_17_083906_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->double('points');
+            $table->integer('user_id');
+            $table->integer('partner_id');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
