fbcd4f71d7080d2dd03e1dea476ee50d4547ce91
aya-matroud <ayamatroud8@gmail.com>
2023-07-20 05:16:04 -0700

    fixed

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -270,7 +270,7 @@ public function getPartners(Request $request)
     if (!is_null($request->avg)) {
         $partners = Partner::select('partners.*')
             ->join(DB::raw('(SELECT partner_id, AVG(points) as avg_points FROM reviews GROUP BY partner_id) as review_avg'), 'partners.id', '=', 'review_avg.partner_id')
-            ->where('review_avg.avg_points', '=', $request->avg)
+            ->where('review_avg.avg_points', '<=', $request->avg)
             ->get();
 
         foreach ($partners as $partner) {
