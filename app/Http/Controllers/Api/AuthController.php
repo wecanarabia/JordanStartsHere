5dfdcf1cff46963c637a623bb5e965e5a8009d64
mina <mina899432@gmail.com>
2023-08-17 14:31:15 +0300

    email

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -461,7 +461,7 @@ class AuthController extends Controller
                     'from' => 'Jordan Starts Here <jordanstartshere@jordanstartshere.com>',
                     'to' => $request->email,
                     'subject' => 'OTP Verification',
-                    'text' => $otp." is your verification code for jordanstartshere.com",
+                    'text' => "$otp is your verification code for jordanstartshere.com",
                 ],
             ]);
             return $response->getBody();
