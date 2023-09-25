bdcb7a48dfe29696b6e5eb67dd91920051b8dfe8
mina <mina899432@gmail.com>
2023-09-25 14:56:11 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -220,12 +220,13 @@ Route::get('partners-range', [PartnerController::class, 'getMinAndMaxOfPrice']);
 //Branch
 Route::get('branches', [BranchController::class, 'list']);
 Route::post('branch-create', [BranchController::class, 'save']);
+//nearbyBranchesIn5 kilometers
+Route::post('branch/nearest', [BranchController::class, 'nearBranch']);
 Route::get('branch/{id}', [BranchController::class, 'view']);
 Route::get('branch/delete/{id}', [BranchController::class, 'delete']);
 Route::post('branch/edit/{id}', [BranchController::class, 'edit']);
 
-//nearbyBranchesIn5 kilometers
-Route::post('branches-nearest', [BranchController::class, 'nearBranch']);
+
 
 
 //portrait image
