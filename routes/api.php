aa33b521455ed5ad34d4116cf54300d5acc11dd6
mina <mina899432@gmail.com>
2023-09-25 15:37:03 +0300

    fix1

--- routes/api.php
+++ routes/api.php
@@ -75,7 +75,7 @@ Route::post('check-otp', [AuthController::class, 'checkOTP']);
 
 Route::middleware('changeLang')->group(function () {
 
-Route::get('nearest', [BranchController::class, 'getnear']);
+Route::get('nearest-branches', [BranchController::class, 'getnear']);
 //Introduction
 Route::get('introductions', [IntroductionController::class, 'list']);
 Route::post('introduction-create', [IntroductionController::class, 'save']);
