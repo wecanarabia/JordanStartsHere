78969433fd6df756e9b6de89edd115539b354b7e
aya-matroud <ayamatroud8@gmail.com>
2023-09-26 02:10:58 -0700

    fixed

--- app/Http/Resources/BranchDistanceResource.php
+++ app/Http/Resources/BranchDistanceResource.php
@@ -38,7 +38,25 @@ class BranchDistanceResource extends JsonResource
             'city_name' => $this->area?->city?->name,
             // 'partner_id' => $this?->partner?->id,
             // 'partner_name' => $this?->partner?->name,
-            'partner'=>new PartnerResource($this->partner)
+            // 'partner'=>new PartnerResource($this->partner),
+            'partner_id' => $this?->partner?->id,
+            'partner_name' => $this?->partner?->name,
+            'partner_logo'=> $this?->partner?->logo,
+            'partner_description' => $this?->partner?->description,
+            'partner_phone' => $this?->partner?->phone,
+            'partner_whatsapp' => $this?->partner?->whatsapp,
+            'partner_video_url' => $this?->partner?->video_url,
+            'partner_file' => $this?->partner?->file,
+            'partner_price_rate' => $this?->partner?->price_rate,
+            'partner_start_price' => $this?->partner?->start_price,
+            'partner_rating'=>(double)$this?->partner?->reviews?->avg('points'),
+            'partner_category' => $this?->partner?->subcategories?->first()?->category?->name ?? null,
+            'partner_category_image' => $this?->partner?->subcategories?->first()?->category?->image ?? null,
+            'partner_branches' => BranchResource::collection($this?->partner?->branches),
+            'partner_portrait_images' => PortraitImageResource::collection($this?->partner?->portraits),
+            'partner_landscape_images' => LandscapeImageResource::collection($this?->partner?->landscapes),
+            'partner_workdays' => WorkdayResource::collection($this?->partner?->workdays),
+            'partner_reviews' => ReviewResource::collection($this?->partner?->reviews),
 
 
 
